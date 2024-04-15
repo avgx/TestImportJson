229a07b792c6e246a6fa926ca0255a544a66af3e
avgx <agovorovsky@gmail.com>
2024-04-15 18:08:31 +0300

    2

--- TestJson/TestJsonApp.swift
+++ TestJson/TestJsonApp.swift
@@ -9,9 +9,44 @@ import SwiftUI
 
 @main
 struct TestJsonApp: App {
+    @UIApplicationDelegateAdaptor private var appDelegate: AppDelegate
+    
+    @State var text: String?
+    
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ContentView(text: text)
+                .onOpenURL { (incomingURL) in
+                        // Handle url here
+                    print(incomingURL.absoluteString)
+                    handleIncomingURL(incomingURL)
+                }
         }
     }
+    
+    private func handleIncomingURL(_ url: URL) {
+        guard url.scheme == "file" else {
+            return
+        }
+        
+        guard let data = try? Data(contentsOf: url) else {
+            return
+        }
+        
+        guard let str = String(data: data, encoding: .utf8) else {
+            return
+        }
+        
+        print("opened content: \(str)")
+        text = str
+    }
+}
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_: UIApplication,
+                     didFinishLaunchingWithOptions _: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool
+    {
+        return true
+    }
+    
 }
