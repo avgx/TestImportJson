50a5168a56247779335e6b5f64887c9dcb19e299
avgx <agovorovsky@gmail.com>
2024-04-15 17:16:23 +0300

    Initial Commit

---
+++ TestJson/TestJsonApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestJsonApp.swift
+//  TestJson
+//
+//  Created by Alexey Govorovsky on 15.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct TestJsonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
